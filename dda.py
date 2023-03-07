add15443e87a0092cf82d39d28ba25d8661d9078
Pedro Lemos <67555838+PedroAmLemos@users.noreply.github.com>
2023-03-07 16:56:36 -0300

    dda

---
+++ dda.py
@@ -0,0 +1,24 @@
+import matplotlib.pyplot as plt
+
+
+def dda(x1, y1, x2, y2, log=0):
+    step = abs(x2 - x1)
+    if abs(y2 - y1) > step:
+        step = abs(y2 - y1)
+
+    xinc = (x2 - x1) / step
+    yinc = (y2 - y1) / step
+    x = x1
+    y = y1
+
+    while x <= x2:
+        plt.scatter(round(x), round(y))
+        if log:
+            print(f"x = {x}, y= {y}, round(y) = {round(y)}")
+        x = x + xinc
+        y = y + yinc
+
+
+if __name__ == '__main__':
+    dda(2, 2, 7, 6)
+    plt.show()
